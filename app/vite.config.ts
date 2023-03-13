466360aeb2c1ce2e012b37ed2227fe8f3716b8d5
Carlos Soriano <soriano.carlitos@gmail.com>
2023-03-13 18:19:20 -0400

    src

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -19,7 +19,7 @@ export default ({ mode }: { mode: string }) => {
     },
     resolve: {
       alias: {
-        "~/": `${path.resolve(__dirname, ".")}/`,
+        "~/": `${path.resolve(__dirname, "src")}/`,
       },
     },
 
--- app/vue.d.ts
+++ app/vue.d.ts
@@ -1,4 +1,4 @@
-declare module '*.vue' {
-  const Component: any
-  export default Component
+declare module "*.vue" {
+  const Component: any;
+  export default Component;
 }
