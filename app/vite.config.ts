88cdc14826ebd9c849037ff4283a5a8a86ba65e5
Carlos Soriano <soriano.carlitos@gmail.com>
2023-03-13 20:58:06 -0400

    add counter component that uses vueuse and it's tests using vitest

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,28 +1,44 @@
 import vue from "@vitejs/plugin-vue";
 import ssr from "vite-plugin-ssr/plugin";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import path from "node:path";
+import { InlineConfig } from "vitest";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+const vitestConfig: InlineConfig = {
+  include: ["test/**/*.test.ts"],
+  environment: "jsdom",
+  deps: {
+    inline: ["@vue", "@vueuse", "vue-demi"],
+  },
+};
 
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
+    plugins: [vue(), ssr()],
+
     server: {
       port: parseInt(process.env.VITE_PORT || "3333"),
       hmr: {
         overlay: false,
       },
     },
+
     build: {
       outDir: "build",
       target: "esnext",
     },
+
     resolve: {
       alias: {
         "~/": `${path.resolve(__dirname, "src")}/`,
       },
     },
 
-    plugins: [vue(), ssr()],
+    test: vitestConfig,
   });
 };
