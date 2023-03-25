d472e1ef3c7bd4cd64caf501f527f21688e093e5
Carlos Soriano <soriano.carlitos@gmail.com>
2023-03-24 21:55:35 -0400

    session cleanup

--- app/worker/api/auth/session.ts
+++ app/worker/api/auth/session.ts
@@ -1,14 +1,33 @@
-// import * as crypt from 'crypto';
-
 import { isValidJwt } from '.';
 import { Env } from '../../types';
 import { createJsonResponse, generateUUID, logLevel } from '../../util';
 import { RequestHandler } from '../RequestHandler';
 import { Session, User } from '../types';
 
-const FILE_LOG_LEVEL = 'debug';
+const FILE_LOG_LEVEL = 'error';
 
 export { handleSession, getSessionFromCookie };
+
+async function clearExpiredSessions(env: Env) {
+  const value = await env.DEMO_CFW_SSR.list();
+  for (const key in value.keys) {
+    if (logLevel(FILE_LOG_LEVEL, env)) {
+      console.log(
+        'worker.handleSsr.value.keys',
+        JSON.stringify(value.keys[key], null, 2),
+      );
+    }
+    if (value.keys[key].expiration < Date.now()) {
+      if (logLevel(FILE_LOG_LEVEL, env)) {
+        console.log(
+          'worker.handleSsr.value.keys.expiration.delete',
+          value.keys[key],
+        );
+      }
+      await env.DEMO_CFW_SSR.delete(value.keys[key].name);
+    }
+  }
+}
 
 async function getSessionFromCookie(
   handler: RequestHandler,
@@ -17,36 +36,41 @@ async function getSessionFromCookie(
 ): Promise<Session | undefined> {
   const cookieName = 'demo-cfw-ssr-session-token';
   const sessionCookie = handler.req.headers.get('Cookie');
-  console.log('worker.handleSsr.sessionCookie', sessionCookie);
+  if (logLevel(FILE_LOG_LEVEL, env)) {
+    console.log('worker.handleSsr.sessionCookie', sessionCookie);
+  }
   let res;
   if (sessionCookie) {
     const sessionToken = sessionCookie.split(`${cookieName}=`)[1];
-    // const sessionTokenB64Decoded = _atob(sessionToken);
-    console.log('worker.handleSsr.sessionToken', sessionToken);
-    // console.log(
-    //   'worker.handleSsr.sessionTokenB64Decoded',
-    //   sessionTokenB64Decoded,
-    // );
+    if (logLevel(FILE_LOG_LEVEL, env)) {
+      console.log('worker.handleSsr.sessionToken', sessionToken);
+    }
     if (sessionToken) {
       const session = await env.DEMO_CFW_SSR.get(sessionToken);
-      console.log('worker.handleSsr.session', session);
-      const value = await env.DEMO_CFW_SSR.list();
-      // console.log('worker.handleSsr.value', value);
-      console.log('worker.handleSsr.value', value.keys);
-      for (const key in value.keys) {
-        console.log(
-          'worker.handleSsr.value.keys',
-          JSON.stringify(value.keys[key], null, 2),
-        );
+      if (logLevel(FILE_LOG_LEVEL, env)) {
+        console.log('worker.handleSsr.session', session);
       }
-
+      const value = await env.DEMO_CFW_SSR.list();
+      if (logLevel(FILE_LOG_LEVEL, env)) {
+        console.log('worker.handleSsr.value', value.keys);
+        for (const key in value.keys) {
+          console.log(
+            'worker.handleSsr.value.keys',
+            JSON.stringify(value.keys[key], null, 2),
+          );
+        }
+      }
       if (session) {
-        const sessionJson = JSON.parse(session);
-        res = sessionJson;
-        // console.log('worker.handleSsr.sessionJson', sessionJson);
-        // if (sessionJson.expires > new Date(Date.now())) {
-        //   user = sessionJson.user;
-        // }
+        const sessionJson: Session = JSON.parse(session);
+        if (sessionJson.expires > new Date(Date.now())) {
+          res = sessionJson;
+          if (logLevel(FILE_LOG_LEVEL, env)) {
+            console.log('worker.handleSsr.sessionJson', sessionJson);
+          }
+          user = sessionJson.user;
+        } else {
+          await env.DEMO_CFW_SSR.delete(sessionToken);
+        }
       }
     }
   }
@@ -79,8 +103,10 @@ async function generateSessionToken(
     const sign = await crypto.subtle.sign('HMAC', secretKey, payloadData);
     const signature = new Uint8Array(sign);
     const signatureBase64 = Buffer.from(signature).toString('base64');
-
-    console.log('signature', signature);
+    if (logLevel(FILE_LOG_LEVEL, env)) {
+      console.log('signature', signature);
+      console.log('signatureBase64', signatureBase64);
+    }
     out = `${payload}.${signatureBase64}`;
   } catch (error) {
     console.log('generateSessionToken.error', error);
@@ -96,6 +122,7 @@ async function handleSession(
   const url = new URL(handler.req.url);
   const method = handler.req.method;
   let res;
+  await clearExpiredSessions(env);
 
   if (url.pathname.startsWith('/api/auth/session')) {
     if (logLevel(FILE_LOG_LEVEL, env)) {
@@ -119,7 +146,9 @@ async function handleSession(
       let session: Session;
       switch (method) {
         case 'GET':
-          console.log('worker.api.auth.session.handleSession.get');
+          if (logLevel(FILE_LOG_LEVEL, env)) {
+            console.log('worker.api.auth.session.handleSession.get');
+          }
           const sessionTokenParam = url.searchParams.get('sessionToken');
           session = JSON.parse(await env.DEMO_CFW_SSR.get(sessionTokenParam));
           res = await handler.handleRequest(env, ctx, session, 200, {
@@ -127,7 +156,9 @@ async function handleSession(
           });
           break;
         case 'POST':
-          console.log('worker.api.auth.session.handleSession.post');
+          if (logLevel(FILE_LOG_LEVEL, env)) {
+            console.log('worker.api.auth.session.handleSession.post');
+          }
           session = await getSessionFromCookie(handler, env);
           if (session) {
             res = await handler.handleRequest(env, ctx, session, 200, {
@@ -139,14 +170,16 @@ async function handleSession(
             crypto.getRandomValues(new Uint8Array(length)).join('');
           const sessionId = generateSessionId(16);
           const sessionToken = await generateSessionToken(env, sessionId);
-          console.log(`sessionToken: 
+          if (logLevel(FILE_LOG_LEVEL, env)) {
+            console.log(`sessionToken: 
           XXXXXXXXXXXXXXXXXXXXX
           ${sessionToken}
           `);
-          console.log(`sessionId:
+            console.log(`sessionId:
           XXXXXXXXXXXXXXXXXXXXX
           ${sessionId}
           `);
+          }
           session = {
             id: sessionId,
             user: handler.user,
@@ -180,7 +213,7 @@ async function handleSession(
       }
     } catch (error) {
       console.error(
-        `worker.api.auth.session.handleSession.error.method ${method}`,
+        `Error: worker.api.auth.session.handleSession.error.method ${method}`,
       );
       console.error(error);
       res = createJsonResponse(
@@ -191,11 +224,12 @@ async function handleSession(
       );
     }
   }
-
-  console.log(
-    'worker.api.auth.session.handleSession.res',
-    JSON.stringify(res, null, 2),
-  );
+  if (logLevel(FILE_LOG_LEVEL, env)) {
+    console.log(
+      'worker.api.auth.session.handleSession.res',
+      JSON.stringify(res, null, 2),
+    );
+  }
 
   return res;
 }
