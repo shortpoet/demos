ca3f041dc30019cc1d9670286221b41ae8f42ce2
Carlos Soriano <soriano.carlitos@gmail.com>
2023-04-01 16:06:29 -0400

    fix empty secret

--- app/scripts/util.ts
+++ app/scripts/util.ts
@@ -1,9 +1,9 @@
-import toml from 'toml';
-import json2toml from 'json2toml';
-import fs from 'node:fs';
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { exec } from 'node:child_process';
+import toml from "toml";
+import json2toml from "json2toml";
+import fs from "node:fs";
+import path, { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { exec } from "node:child_process";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -23,7 +23,7 @@ function command(cmd): Promise<string> {
 function getToml(): Record<string, any> {
   try {
     return toml.parse(
-      fs.readFileSync(path.join(__dirname, '../../wrangler.toml'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, "../../wrangler.toml"), "utf8")
     );
   } catch (error) {
     console.error(error);
@@ -33,14 +33,14 @@ function getToml(): Record<string, any> {
 const writeToml = (data: any) => {
   try {
     fs.writeFileSync(
-      path.join(__dirname, '../../wrangler.bak.toml'),
-      fs.readFileSync(path.join(__dirname, '../../wrangler.toml'), 'utf8'),
+      path.join(__dirname, "../../wrangler.bak.toml"),
+      fs.readFileSync(path.join(__dirname, "../../wrangler.toml"), "utf8")
     );
     // console.log('wrote toml');
     // console.log(data);
     fs.writeFileSync(
-      path.join(__dirname, '../../wrangler.toml'),
-      json2toml(data),
+      path.join(__dirname, "../../wrangler.toml"),
+      json2toml(data)
     );
   } catch (error) {
     console.error(error);
@@ -57,7 +57,11 @@ const writeFile = async (file: string, data: string) => {
 
 const readFile = async (file: string) => {
   try {
-    return await fs.promises.readFile(file, 'utf8');
+    if (fs.existsSync(file)) {
+      return await fs.promises.readFile(file, "utf8");
+    } else {
+      return "";
+    }
   } catch (error) {
     console.error(error);
   }
